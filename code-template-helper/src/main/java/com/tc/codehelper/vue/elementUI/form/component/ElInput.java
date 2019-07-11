8ad53444cab5ae35a0b31b7e23b970a6b46160ad
tangcheng <3276795971@qq.com>
2019-07-11 16:22:06 +0800

    通过组建type确定表单的控件

--- code-template-helper/src/main/java/com/tc/codehelper/vue/elementUI/form/component/ElInput.java
+++ code-template-helper/src/main/java/com/tc/codehelper/vue/elementUI/form/component/ElInput.java
@@ -3,14 +3,14 @@ package com.tc.codehelper.vue.elementUI.form.component;
 public class ElInput extends ElFormInputComponent {
 
     public ElInput() {
-        super("input");
+        super("el-input");
     }
 
     public ElInput(String name) {
-        super("input", name);
+        super("el-input", name);
     }
 
     public ElInput(String name, String placeholder) {
-        super("input", name, placeholder);
+        super("el-input", name, placeholder);
     }
 }
---
+++ code-template-helper/src/main/java/com/tc/codehelper/vue/elementUI/form/component/ElInputNumber.java
@@ -0,0 +1,16 @@
+package com.tc.codehelper.vue.elementUI.form.component;
+
+public class ElInputNumber extends ElFormInputComponent {
+
+    public ElInputNumber() {
+        super("el-input-number");
+    }
+
+    public ElInputNumber(String name) {
+        super("el-input-number", name);
+    }
+
+    public ElInputNumber(String name, String placeholder) {
+        super("el-input-number", name, placeholder);
+    }
+}
